754c50f20ec6b292787831dad5387034502da343
Andrey Fedorov <info@ndru.tv>
2022-07-20 13:49:02 +0400

    Обновил функцию через новую переменную + комменты

--- colorFlipper/js/main.js
+++ colorFlipper/js/main.js
@@ -1,11 +1,17 @@
+// определение переменных
 const colors = ['red', 'blue', 'yellow', 'green', 'aqua'];
+const txtColorName = document.querySelector("#txtColorName");
 
+// функция смены цвета
 const changeColor = () => {
+    // рандомайзер на основе массива данных
     let randomColor = colors[getRandom(colors.length)];
     
+    // изменение стилей
     document.body.style.backgroundColor = randomColor;
-    document.querySelector("#color-name").innerHTML = randomColor;
-    document.querySelector("#color-name").style.color = randomColor;
+    txtColorName.innerHTML = randomColor;
+    txtColorName.style.color = randomColor;
+    txtColorName.style.textTransform = 'capitalize';
 }
 
 // const changeColorByRgb = () => {
@@ -18,7 +24,8 @@ const changeColor = () => {
 //     document.body.style.backgroundColor = ('#' + hex);
 // }
 
+// активация функции по нажатии на кнопку
 document.querySelector('#btnChangeColor').addEventListener(
-    'click', 
+    'click',
     changeColor
 );
--- colorFlipper/js/utils.js
+++ colorFlipper/js/utils.js
@@ -1,3 +1,4 @@
+// рандомайзер
 const getRandom = (max) => Math.floor(Math.random() * max);
 
 // const getRandomHex = (modificator) => Math.floor(Math.random() * (modificator || 16777215)).toString(16);
