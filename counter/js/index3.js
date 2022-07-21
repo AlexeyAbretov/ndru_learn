536864eac348f5d92695490f3cb7ce6e0035feaa
Абретов Алексей Александрович (4052233) <aabretov@vtb.ru>
2022-07-21 20:53:14 +0300

    feat: add counter example
    - add counter type #3
    
    meta: learn

---
+++ counter/js/index3.js
@@ -0,0 +1,32 @@
+const counterClick = (e) => {
+    const counter = e.currentTarget.closest('.counter');
+    const txtCounterValue = counter.querySelector('div[name=txtCounterValue]');
+    
+    console.log(e.target, e.currentTarget);
+    
+    if (e.target.name === 'btnDecrease') {
+        decrease(txtCounterValue);
+    } else if (e.target.name === 'btnReset') {
+        reset(txtCounterValue);
+    } else if (e.target.name === 'btnIncrease') {
+        increase(txtCounterValue);
+    }
+}
+
+document.querySelectorAll(
+    '.buttons'
+).forEach((el) => 
+    el.addEventListener('click', counterClick)
+);
+
+const decrease = (txtCounterValue) => {
+    txtCounterValue.textContent = Number(txtCounterValue.textContent || '0') - 1;
+}
+
+const reset = (txtCounterValue) => {
+    txtCounterValue.textContent = 0;
+}
+
+const increase = (txtCounterValue) => {
+    txtCounterValue.textContent = Number(txtCounterValue.textContent || '0') + 1;
+}
