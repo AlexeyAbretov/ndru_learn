29daa40a517c88dc43fede2781361f0122198e4e
Andrey Fedorov <info@ndru.tv>
2022-07-22 04:31:21 +0400

    Эксперименты с атрибутами

--- counter/js/index5.js
+++ counter/js/index5.js
@@ -1,11 +1,11 @@
-const createCounter = (id) => {
-    const counter = document.getElementById('counter').content.cloneNode(true);
+// const createCounter = (id) => {
+//     const counter = document.getElementById('counter').content.cloneNode(true);
 
-    counter.querySelector('.counter').setAttribute('id', id);
-    counter.querySelector('.buttons').addEventListener('click', counterClick);
+//     counter.querySelector('.counter').setAttribute('id', id);
+//     counter.querySelector('.buttons').addEventListener('click', counterClick);
 
-    document.getElementById('root').append(counter);
-}
+//     document.getElementById('root').append(counter);
+// }
 
 const counterClick = (e) => {
     const counter = e.currentTarget.closest('.counter');
@@ -17,23 +17,41 @@ const counterClick = (e) => {
         reset(txtCounterValue);
     } else if (e.target.name === 'btnStart') {
         start(txtCounterValue);
+    } else if (e.target.name === 'btnStop') {
+        stop(txtCounterValue);
     }
-}
+};
 
 let intervalId = {};
+const btnChange = document.querySelector('button[name=btnStart]');
 
 const reset = (txtCounterValue) => {
     const counter = txtCounterValue.closest('.counter');
     
-    clearInterval(intervalId[counter.id]);
+    clearInterval(intervalId[counter]);
 
     txtCounterValue.textContent = 0;
-}
+
+    btnChange.textContent = 'Start';
+    btnChange.setAttribute('name', 'btnStart');
+};
 
 const start = (txtCounterValue) => {
     const counter = txtCounterValue.closest('.counter');
 
-    intervalId[counter.id] = setInterval(() => {
+    intervalId[counter] = setInterval(() => {
         txtCounterValue.textContent = Number(txtCounterValue.textContent || '0') + 1;
-    }, 1000)
-}
+    }, 1000);
+
+    btnChange.textContent = 'Stop';
+    btnChange.setAttribute('name', 'btnStop');
+};
+
+const stop = (txtCounterValue) => {
+    const counter = txtCounterValue.closest('.counter');
+    
+    clearInterval(intervalId[counter]);
+
+    btnChange.textContent = 'Start';
+    btnChange.setAttribute('name', 'btnStart');
+};
