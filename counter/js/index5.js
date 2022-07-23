d1b0824588854c9c2a6ac9a55bc5e1b26a27989c
AlexeyAbretov <alexey.abretov@gmail.com>
2022-07-23 12:27:29 +0300

    feat(auto counter): Старт/Стоп на одной кнопке

--- counter/js/index5.js
+++ counter/js/index5.js
@@ -1,12 +1,3 @@
-// const createCounter = (id) => {
-//     const counter = document.getElementById('counter').content.cloneNode(true);
-
-//     counter.querySelector('.counter').setAttribute('id', id);
-//     counter.querySelector('.buttons').addEventListener('click', counterClick);
-
-//     document.getElementById('root').append(counter);
-// }
-
 const counterClick = (e) => {
     const counter = e.currentTarget.closest('.counter');
     const txtCounterValue = counter.querySelector('div[name=txtCounterValue]');
@@ -15,43 +6,36 @@ const counterClick = (e) => {
     
     if (e.target.name === 'btnReset') {
         reset(txtCounterValue);
-    } else if (e.target.name === 'btnStart') {
+    } else if (!intervalId) {
         start(txtCounterValue);
-    } else if (e.target.name === 'btnStop') {
-        stop(txtCounterValue);
+    } else {
+        stop();
     }
 };
 
-let intervalId = {};
-const btnChange = document.querySelector('button[name=btnStart]');
+let intervalId = null;
+const btnChange = document.querySelector('button[name=btnStartOrStop]');
 
 const reset = (txtCounterValue) => {
-    const counter = txtCounterValue.closest('.counter');
-    
-    clearInterval(intervalId[counter]);
+    clearInterval(intervalId);
+    intervalId = null;
 
     txtCounterValue.textContent = 0;
 
     btnChange.textContent = 'Start';
-    btnChange.setAttribute('name', 'btnStart');
 };
 
 const start = (txtCounterValue) => {
-    const counter = txtCounterValue.closest('.counter');
-
-    intervalId[counter] = setInterval(() => {
+    intervalId = setInterval(() => {
         txtCounterValue.textContent = Number(txtCounterValue.textContent || '0') + 1;
     }, 1000);
 
     btnChange.textContent = 'Stop';
-    btnChange.setAttribute('name', 'btnStop');
 };
 
-const stop = (txtCounterValue) => {
-    const counter = txtCounterValue.closest('.counter');
-    
-    clearInterval(intervalId[counter]);
+const stop = () => {
+    clearInterval(intervalId);
+    intervalId = null;
 
     btnChange.textContent = 'Start';
-    btnChange.setAttribute('name', 'btnStart');
 };
