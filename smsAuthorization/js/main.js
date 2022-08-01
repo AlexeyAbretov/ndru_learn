c139ef92775b7c8098c50a1b65eb0ab0efffa372
Andrey Fedorov <info@ndru.tv>
2022-08-01 22:46:07 +0400

    add smsAuthorization project

---
+++ smsAuthorization/js/main.js
@@ -0,0 +1,59 @@
+// настройка
+let timeInSeconds = 30;
+
+// определяем переменные переменные
+let btnRequest = document.querySelector('.request-code');
+let requestTimer = document.querySelector('.sms__counter');
+let timer = document.querySelector('.times');
+let inputSmsCode = document.querySelector('.input-code');
+let btnShowHidePassword = document.querySelector('.password-control');
+let timerId = null;
+
+// нажатие на "Запросить код"
+let requestCode = () => {
+    
+    addRemoveClass();
+    countDown();
+};
+
+// добавление и удаление классов
+let addRemoveClass = () => {
+    btnRequest.setAttribute('disabled', true);
+    btnRequest.classList.add('button_disabled');
+    requestTimer.classList.add('_visibility');
+
+    setTimeout(() => {
+        btnRequest.removeAttribute('disabled');
+        btnRequest.classList.remove('button_disabled');
+        requestTimer.classList.remove('_visibility');
+    },
+    timeInSeconds*1000);
+};
+
+// отсчет времени
+let countDown = () => {
+    let count = timeInSeconds;
+    timer.textContent = ` ${count}`;
+    
+    let timerId = setInterval(() => {
+        if ( count > 0 ) {
+            count--;
+            timer.textContent = ` ${count}`;
+        } else {
+            count = timeInSeconds;
+
+            clearInterval(timerId);
+        }
+    }, 1000);
+};
+
+// показать/скрыть код
+let showHidePassword = () => {
+    if ( inputSmsCode.type === 'password' ) {
+        inputSmsCode.setAttribute('type', 'text');
+        btnShowHidePassword.classList.add('password-control_hide');
+    } else {
+        inputSmsCode.setAttribute('type', 'password');
+        btnShowHidePassword.classList.remove('password-control_hide');
+    }
+};
