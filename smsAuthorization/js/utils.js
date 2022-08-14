b6268981e715f79672cd2087c43691f802c8c525
Andrey Fedorov <info@ndru.tv>
2022-08-15 00:50:51 +0400

    add some modal fixes

--- smsAuthorization/js/utils.js
+++ smsAuthorization/js/utils.js
@@ -1,3 +1,36 @@
+// Модальные окна
+document.addEventListener('DOMContentLoaded', () => {
+    const modalButtons = document.querySelectorAll('.js-open-modal'),
+          overlay      = document.querySelector('.js-overlay-modal'),
+          closeButtons = document.querySelectorAll('.js-modal-close');
+ 
+    modalButtons.forEach(item => {
+        item.addEventListener('click', (e) => {
+            e.preventDefault();
+ 
+            let modalId = e.target.getAttribute('data-modal'),
+            modalElem = document.querySelector(`.modal[data-modal="${modalId}"]`);
+    
+            modalElem.classList.add('active');
+            overlay.classList.add('active');
+        }); // end click
+    }); // end foreach
+ 
+    closeButtons.forEach(item => {
+        item.addEventListener('click', (e) => {
+            let parentModal = e.target.closest('.modal');
+    
+            parentModal.classList.remove('active');
+            overlay.classList.remove('active');
+        }); // end click
+    }); // end foreach
+ 
+    overlay.addEventListener('click', (e) => {
+        document.querySelector('.modal.active').classList.remove('active');
+        e.target.classList.remove('active');
+    }); // end click
+});
+
 // let transformTime = (start) => {
 //     let minutes = Math.floor(start / 60);
 //     let seconds = start % 60;
